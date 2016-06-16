f6971d07977d7034cedfe7e03d3704afd988b018
chriswebb09 <chris.webb5249@gmail.com>
2016-06-15 20:42:04 -0400

    Done.

---
+++ objc-PetCast/FISDog.m
@@ -0,0 +1,21 @@
+//
+//  FISDog.m
+//  objc-PetCast
+//
+//  Created by Christopher Webb-Orenstein on 6/15/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISDog.h"
+
+@implementation FISDog
+
+-(NSString *)assaultTheMailman {
+    return @"I got the mail! I got the mail!";
+}
+
+-(NSString *)makeASound {
+    return @"Woof!";
+}
+
+@end
